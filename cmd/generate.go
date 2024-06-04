527ce496982f0e92c99b0b363eae73aed35bfeff
Abhijit Roy <roya51788@gmail.com>
2024-06-04 13:52:35 +0530

    Added dependency cpuguy

--- cmd/generate.go
+++ cmd/generate.go
@@ -55,22 +55,47 @@ package cmd
 import (
     "fmt"
     "os"
+    "path/filepath"
     "text/template"
 
+    "github.com/AlecAivazis/survey/v2"
     "github.com/spf13/cobra"
 )
 
-var (
-    licenseType string
-    year        string
-    name        string
-)
+var year string
+var name string
 
 var generateCmd = &cobra.Command{
     Use:   "generate",
     Short: "Generate a license file",
     Run: func(cmd *cobra.Command, args []string) {
-        if err := generateLicense(licenseType, year, name); err != nil {
+        // Get available templates
+        templates, err := getTemplates("templates")
+        if err != nil {
+            fmt.Println("Error retrieving templates:", err)
+            return
+        }
+
+        // If no templates found, return an error
+        if len(templates) == 0 {
+            fmt.Println("No templates found.")
+            return
+        }
+
+        // Prompt user to select a template
+        var selectedTemplate string
+        prompt := &survey.Select{
+            Message: "Choose a template:",
+            Options: templates,
+        }
+        err = survey.AskOne(prompt, &selectedTemplate)
+        if err != nil {
+            fmt.Println("Error selecting template:", err)
+            return
+        }
+
+        // Generate the license file with the selected template
+        if err := generateLicense(selectedTemplate, year, name); err != nil {
             fmt.Println("Error:", err)
         }
     },
@@ -78,15 +103,27 @@ var generateCmd = &cobra.Command{
 
 func init() {
     rootCmd.AddCommand(generateCmd)
-    generateCmd.Flags().StringVarP(&licenseType, "type", "t", "", "Type of license (e.g., MIT, Apache)")
     generateCmd.Flags().StringVarP(&year, "year", "y", "2024", "Year for the license")
     generateCmd.Flags().StringVarP(&name, "name", "n", "Your Name", "Name for the license")
-    generateCmd.MarkFlagRequired("type")
 }
 
-func generateLicense(licenseType, year, name string) error {
-    tmpl, err := template.ParseFiles(fmt.Sprintf("templates/%s.tmpl", licenseType))
-    if (err != nil) {
+func getTemplates(dir string) ([]string, error) {
+    var templates []string
+    err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() && filepath.Ext(path) == ".tmpl" {
+            templates = append(templates, filepath.Base(path))
+        }
+        return nil
+    })
+    return templates, err
+}
+
+func generateLicense(templateFile, year, name string) error {
+    tmpl, err := template.ParseFiles(fmt.Sprintf("templates/%s", templateFile))
+    if err != nil {
         return fmt.Errorf("loading template: %w", err)
     }
 
@@ -99,7 +136,7 @@ func generateLicense(licenseType, year, name string) error {
     }
 
     file, err := os.Create("LICENSE")
-    if (err != nil) {
+    if err != nil {
         return fmt.Errorf("creating file: %w", err)
     }
     defer file.Close()
@@ -111,3 +148,4 @@ func generateLicense(licenseType, year, name string) error {
     fmt.Println("License file generated successfully")
     return nil
 }
+
--- go.mod
+++ go.mod
@@ -2,8 +2,17 @@ module license-generator
 
 go 1.22.3
 
+require github.com/spf13/cobra v1.8.0
+
 require (
+	github.com/AlecAivazis/survey/v2 v2.3.7 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
