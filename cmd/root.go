f8aca8fba610f9cba93108d79dc877a05f93b422
Abhijit Roy <roya51788@gmail.com>
2024-06-08 00:12:27 +0530

    add:11 more license & Updated prompts for year and name only when needed

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,10 @@ var rootCmd = &cobra.Command{
         var name string
 
         // Define available templates
-        templates := []string{"MIT", "Apache"}
+        templates := []string{
+            "MIT", "Apache", "AGPL", "BSD-2-Clause", "BSD-3-Clause", "Boost",
+            "CC0", "EPL", "GPL-2.0", "GPL-3.0", "LGPL-2.1", "MPL", "Unlicense",
+        }
 
         // Prompt user to select a template
         var selectedTemplate string
@@ -29,26 +32,41 @@ var rootCmd = &cobra.Command{
             return
         }
 
-        // Prompt user for the year
-        yearPrompt := &survey.Input{
-            Message: "Enter the year:",
-            Default: "2024",
-        }
-        err = survey.AskOne(yearPrompt, &year)
-        if err != nil {
-            fmt.Println("Error entering year:", err)
-            return
+        // Templates that require year and name
+        templatesWithYearName := map[string]bool{
+            "MIT":           true,
+            "Apache":        true,
+            "AGPL":          true,
+            "BSD-2-Clause":  true,
+            "BSD-3-Clause":  true,
+            "Boost":         true,
+            "GPL-2.0":       true,
+            "GPL-3.0":       true,
+            "LGPL-2.1":      true,
         }
 
-        // Prompt user for the name
-        namePrompt := &survey.Input{
-            Message: "Enter your name:",
-            Default: "Your Name",
-        }
-        err = survey.AskOne(namePrompt, &name)
-        if err != nil {
-            fmt.Println("Error entering name:", err)
-            return
+        // Prompt user for the year if needed
+        if templatesWithYearName[selectedTemplate] {
+            yearPrompt := &survey.Input{
+                Message: "Enter the year:",
+                Default: "2024",
+            }
+            err = survey.AskOne(yearPrompt, &year)
+            if err != nil {
+                fmt.Println("Error entering year:", err)
+                return
+            }
+
+            // Prompt user for the name if needed
+            namePrompt := &survey.Input{
+                Message: "Enter your name:",
+                Default: "Your Name",
+            }
+            err = survey.AskOne(namePrompt, &name)
+            if err != nil {
+                fmt.Println("Error entering name:", err)
+                return
+            }
         }
 
         // Generate the license file with the selected template
@@ -65,3 +83,4 @@ func Execute() error {
 func init() {
     // No longer setting default values for year and name
 }
+
